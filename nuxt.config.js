a18aa4c0461780c400733f3e54abde9c7a5ca5ae
Joshua <joshuatundag@gmail.com>
2020-12-24 15:44:59 +0800

    (wip) feat: added tags field

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,8 @@ module.exports = {
    ** Plugins to load before mounting the App
    */
   plugins: [
-    { src: '~/plugins/vuex-persist', ssr: false },
+    { src: '~/plugins/vuex-persist.js', ssr: false },
+    { src: '~/plugins/vue-tags-input.js', ssr: false },
     "~/plugins/prototypes.js",
     "~/plugins/external-installs.js",
     "~/plugins/api.js",
