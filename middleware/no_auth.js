8c71218d0de9dc38bf86fc3953f593280d3d56e8
Joshua <joshuatundag@gmail.com>
2020-12-26 19:12:20 +0800

    fix: logout error

--- middleware/no_auth.js
+++
@@ -1,5 +0,0 @@
-export default function({store, route, redirect, isServer, $auth}){
-  const user = store.getters['user']
-  const isLoggedIn = store.getters['isLoggedIn']
-  if(isLoggedIn) return redirect('/dashboard/collections')
-}
